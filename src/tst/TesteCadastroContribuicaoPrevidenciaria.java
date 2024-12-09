8090bd008f7d7d28246a8e8dc3fbc9b475277c58
Lais Portela <lais3722@gmail.com>
2024-12-09 19:02:48 -0300

    Parametrizando dois testes

--- src/tst/TesteCadastroContribuicaoPrevidenciaria.java
+++ src/tst/TesteCadastroContribuicaoPrevidenciaria.java
@@ -1,15 +1,36 @@
 package tst;
 
-import static org.junit.Assert.assertEquals;
-
+import app.IRPF;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+import java.util.Collection;
 
-import app.IRPF;
-
+@RunWith(Parameterized.class)
 public class TesteCadastroContribuicaoPrevidenciaria {
 
     IRPF irpf;
+    private final int[] contribuicoes;
+    private final int numcontribuicoesEsperadas;
+    private float totalcontribuicoesEsperadas;
+
+    public TesteCadastroContribuicaoPrevidenciaria(int[] contribuicoes, int numcontribuicoesEsperadas, float totalcontribuicoesEsperadas) {
+        this.contribuicoes = contribuicoes;
+        this.numcontribuicoesEsperadas = numcontribuicoesEsperadas;
+        this.totalcontribuicoesEsperadas = totalcontribuicoesEsperadas;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                { new int[]{1000}, 1, 1000f },
+                { new int[]{1000, 500}, 2, 1500f },
+                { new int[]{1000, 500, 200}, 3, 1700f }
+        });
+    }
 
     @Before
     public void setup() {
@@ -17,31 +38,12 @@ public class TesteCadastroContribuicaoPrevidenciaria {
     }
 
     @Test
-    public void cadastrarUmaContribuicaoPrevidenciaria() {
-        irpf.cadastrarContribuicaoPrevidenciaria(1000);
-        assertEquals(1, irpf.getNumContribuicoesPrevidenciarias());
-        assertEquals(1000f, irpf.getTotalContribuicoesPrevidenciarias(), 0f);
-        assertEquals(1000f, irpf.getDeducao(), 0f);
+    public void testCadastroContribuicoesPrevidenciarias() {
+        for (int contribuicao : contribuicoes) {
+            irpf.cadastrarContribuicaoPrevidenciaria(contribuicao);
+        }
+        assertEquals(numcontribuicoesEsperadas, irpf.getNumContribuicoesPrevidenciarias());
+        assertEquals(totalcontribuicoesEsperadas, irpf.getTotalContribuicoesPrevidenciarias(), 0f);
+        assertEquals(totalcontribuicoesEsperadas, irpf.getDeducao(), 0f);
     }
-
-
-    @Test
-    public void cadastrarDuasContribuicoesPrevidenciarias() {
-        irpf.cadastrarContribuicaoPrevidenciaria(1000);
-        irpf.cadastrarContribuicaoPrevidenciaria(500);
-        assertEquals(2, irpf.getNumContribuicoesPrevidenciarias());
-        assertEquals(1500f, irpf.getTotalContribuicoesPrevidenciarias(), 0f);
-        assertEquals(1500f, irpf.getDeducao(), 0f);
-    }
-
-    @Test
-    public void cadastrarTresContribuicoesPrevidenciarias() {
-        irpf.cadastrarContribuicaoPrevidenciaria(1000);
-        irpf.cadastrarContribuicaoPrevidenciaria(500);
-        irpf.cadastrarContribuicaoPrevidenciaria(200);
-        assertEquals(3, irpf.getNumContribuicoesPrevidenciarias());
-        assertEquals(1700f, irpf.getTotalContribuicoesPrevidenciarias(), 0f);
-        assertEquals(1700f, irpf.getDeducao(), 0f);
-    }
-
 }
--- src/tst/TesteRendimentos.java
+++ src/tst/TesteRendimentos.java
@@ -1,15 +1,39 @@
 package tst;
 
 import static org.junit.Assert.*;
-
 import org.junit.Before;
 import org.junit.Test;
-
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import java.util.Arrays;
+import java.util.Collection;
 import app.IRPF;
 
+@RunWith(Parameterized.class)
 public class TesteRendimentos {
-    
-    IRPF irpf;
+
+    private IRPF irpf;
+    private Object[][] rendimentos;
+    private int numRendimentosEsperado;
+    private float totalRendimentosEsperado;
+    private float totalRendimentosTributaveisEsperado;
+
+    public TesteRendimentos(Object[][] rendimentos, int numRendimentosEsperado, float totalRendimentosEsperado, float totalRendimentosTributaveisEsperado) {
+        this.rendimentos = rendimentos;
+        this.numRendimentosEsperado = numRendimentosEsperado;
+        this.totalRendimentosEsperado = totalRendimentosEsperado;
+        this.totalRendimentosTributaveisEsperado = totalRendimentosTributaveisEsperado;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                { new Object[][] {{"Salario", IRPF.TRIBUTAVEL, 5000f}}, 1, 5000f, 5000f },
+                { new Object[][] {{"Aluguel", IRPF.TRIBUTAVEL, 4000f}}, 1, 4000f, 4000f },
+                { new Object[][] {{"Salario", IRPF.TRIBUTAVEL, 5000f}, {"Aluguel", IRPF.TRIBUTAVEL, 4000f}}, 2, 9000f, 9000f },
+                { new Object[][] {{"Salario", IRPF.TRIBUTAVEL, 5000f}, {"Aluguel", IRPF.TRIBUTAVEL, 4000f}, {"Bolsa de pesquisa", IRPF.NAOTRIBUTAVEL, 3000f}}, 3, 12000f, 9000f }
+        });
+    }
 
     @Before
     public void setup() {
@@ -17,38 +41,16 @@ public class TesteRendimentos {
     }
 
     @Test
-    public void test1RendimentoTributavel() {
-        irpf.criarRendimento("Salario", IRPF.TRIBUTAVEL, 5000f);
-        assertEquals(1, irpf.getNumRendimentos());
-        assertEquals(5000f, irpf.getTotalRendimentos(), 0);
-        assertEquals(5000f, irpf.getTotalRendimentosTributaveis(), 0);
-    }
+    public void testRendimentos() {
+        for (Object[] rendimento : rendimentos) {
+            String nomeRendimento = (String) rendimento[0];
+            boolean tributavel = (boolean) rendimento[1];
+            float valor = (float) rendimento[2];
+            irpf.criarRendimento(nomeRendimento, tributavel, valor);
+        }
 
-    @Test
-    public void testeOutroRendimentoTributavel() {
-        irpf.criarRendimento("Aluguel", IRPF.TRIBUTAVEL, 4000f);
-        assertEquals(1, irpf.getNumRendimentos());
-        assertEquals(4000f, irpf.getTotalRendimentos(), 0);
-        assertEquals(4000f, irpf.getTotalRendimentosTributaveis(), 0);
+        assertEquals(numRendimentosEsperado, irpf.getNumRendimentos());
+        assertEquals(totalRendimentosEsperado, irpf.getTotalRendimentos(), 0);
+        assertEquals(totalRendimentosTributaveisEsperado, irpf.getTotalRendimentosTributaveis(), 0);
     }
-
-    @Test
-    public void testDoisRendimentosTributaveis() {
-        irpf.criarRendimento("Salario", IRPF.TRIBUTAVEL, 5000f);
-        irpf.criarRendimento("Aluguel", IRPF.TRIBUTAVEL, 4000f);
-        assertEquals(2, irpf.getNumRendimentos());
-        assertEquals(9000f, irpf.getTotalRendimentos(), 0);
-        assertEquals(9000f, irpf.getTotalRendimentosTributaveis(), 0);
-    }
-
-    @Test
-    public void testeTresRendimentos() {
-        irpf.criarRendimento("Salario", IRPF.TRIBUTAVEL, 5000f);
-        irpf.criarRendimento("Aluguel", IRPF.TRIBUTAVEL, 4000f);
-        irpf.criarRendimento("Bolsa de pesquisa", IRPF.NAOTRIBUTAVEL, 3000f);
-        assertEquals(3, irpf.getNumRendimentos());
-        assertEquals(12000f, irpf.getTotalRendimentos(), 0);
-        assertEquals(9000f, irpf.getTotalRendimentosTributaveis(), 0);
-    }
-
 }
