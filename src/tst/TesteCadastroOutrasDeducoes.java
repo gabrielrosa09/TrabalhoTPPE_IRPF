bd06becc4cf7338f7ef68a9754f4d6affc9f7304
Hellen159 <202016480@aluno.unb.br>
2024-12-10 11:07:04 -0300

    Refatorando "TesteCadastroOutrasDeducoes" para ser usado parametros.

--- src/tst/TesteCadastroOutrasDeducoes.java
+++ src/tst/TesteCadastroOutrasDeducoes.java
@@ -8,11 +8,48 @@ import org.junit.Before;
 import org.junit.Test;
 
 import app.IRPF;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import app.IRPF;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
 public class TesteCadastroOutrasDeducoes {
 
-    IRPF irpf;
+    private IRPF irpf;
+    private String[] deducoes;
+    private float[] valores;
+    private float totalEsperado;
+    private String consultaDeducao;
+    private Float valorEsperadoConsulta;
+    
+    public TesteCadastroOutrasDeducoes(String[] deducoes, float[] valores, float totalEsperado, String consultaDeducao, Float valorEsperadoConsulta) {
+        this.deducoes = deducoes;
+        this.valores = valores;
+        this.totalEsperado = totalEsperado;
+        this.consultaDeducao = consultaDeducao;
+        this.valorEsperadoConsulta = valorEsperadoConsulta;
+    }
 
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {new String[]{"Funpresp"}, new float[]{1000f}, 1000f, "Funpresp", 1000f},
+                {new String[]{"Funpresp", "Carne-leao"}, new float[]{1000f, 500f}, 1500f, "Carne-leao", 500f},
+                {new String[]{"Funpresp", "Carne-leao", "PGBL"}, new float[]{1000f, 500f, 500f}, 2000f, "PGBL", 500f},
+                {new String[]{"Funpresp"}, new float[]{1000f}, 1000f, "Carne-leao", null} 
+        });
+    }
+    
+    
     @Before
     public void setup() {
         irpf = new IRPF();
@@ -20,43 +57,19 @@ public class TesteCadastroOutrasDeducoes {
 
 
     @Test
-    public void cadastroUmaDeducao() {
-        irpf.cadastrarDeducaoIntegral("Funpresp", 1000f);
-        assertNotNull(irpf.getOutrasDeducoes("Funpresp"));
-        assertEquals(1000f, irpf.getDeducao("Funpresp"), 0f);
-        assertEquals(1000f, irpf.getTotalOutrasDeducoes(), 0f);
-    }
+    public void testCadastroOutrasDeducoes() {
+        for (int i = 0; i < deducoes.length; i++) {
+            irpf.cadastrarDeducaoIntegral(deducoes[i], valores[i]);
+        }
 
-    @Test
-    public void cadastroDuasDeducoes() {
-        irpf.cadastrarDeducaoIntegral("Funpresp", 1000f);
-        irpf.cadastrarDeducaoIntegral("Carne-leao", 500f);
-        assertNotNull(irpf.getOutrasDeducoes("Funpresp"));
-        assertEquals(1000f, irpf.getDeducao("Funpresp"), 0f);
-        assertEquals(500f, irpf.getDeducao("Carne-leao"), 0f);
-        assertEquals(1500f, irpf.getTotalOutrasDeducoes(), 0f);
-    }
+        if (valorEsperadoConsulta == null) {
+            assertNull(irpf.getOutrasDeducoes(consultaDeducao));
+            assertEquals(0f, irpf.getDeducao(consultaDeducao), 0f);
+        } else {
+            assertNotNull(irpf.getOutrasDeducoes(consultaDeducao));
+            assertEquals(valorEsperadoConsulta, irpf.getDeducao(consultaDeducao), 0f);
+        }
 
-    @Test
-    public void cadastroTresDeducoes() {
-        irpf.cadastrarDeducaoIntegral("Funpresp", 1000f);
-        irpf.cadastrarDeducaoIntegral("Carne-leao", 500f);
-        irpf.cadastrarDeducaoIntegral("PGBL", 500f);
-        assertNotNull(irpf.getOutrasDeducoes("Funpresp"));
-        assertEquals(1000f, irpf.getDeducao("Funpresp"), 0f);
-        assertEquals(500f, irpf.getDeducao("Carne-leao"), 0f);
-        assertEquals(500f, irpf.getDeducao("PGBL"), 0f);
-        assertEquals(2000f, irpf.getTotalOutrasDeducoes(), 0f);
-    }
-
-    @Test
-    public void deducaoInexistente() {
-        String nomeDeducao = "Funpresp";
-        float valorDeducao = 1000f;
-        irpf.cadastrarDeducaoIntegral(nomeDeducao, valorDeducao);
-
-
-        assertNull(irpf.getOutrasDeducoes("Carne-leao"));
-        assertEquals(0f, irpf.getDeducao("Carne-leao"), 0f);
+        assertEquals(totalEsperado, irpf.getTotalOutrasDeducoes(), 0f);
     }
 }
